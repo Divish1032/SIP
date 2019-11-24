fa41348292c96130be4a717441c6e908735c4608
Divish1032 <kumar.divansh1996@gmail.com>
2019-11-24 18:39:05 +0530

    F

--- routes/intern-job.js
+++ routes/intern-job.js
@@ -27,7 +27,7 @@ router.get("/", middleware.isLoggedIn, function(req, res){
            Internships.sort(function(a,b){
             // Turn your strings into dates, and then subtract them
             // to get a value that is either negative, positive, or zero.
-            return new Date(b.date) - new Date(a.date);
+            return a.date.toDateString() - b.date.toDateString();
             });
 
            // res.render("campgrounds/index", {campgrounds : campgrounds, currentUser : req.user});
